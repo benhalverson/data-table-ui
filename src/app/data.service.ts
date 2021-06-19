058f7ace5d3b6f752525b2ccef2fa1080633b23c
Ben Halverson <benhalverson33@gmail.com>
2021-06-18 22:43:12 -0700

    added table service and component

---
+++ src/app/data.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Table } from './table';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor() {}
+  inMemoryData() {
+    const data: Table[] = [
+      {
+        name: 'smss.exe',
+        device: 'Stark',
+        path: '\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe',
+        status: 'scheduled',
+      },
+      {
+        name: 'netsh.exe',
+        device: 'Targaryen',
+        path: '\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe',
+        status: 'available',
+      },
+      {
+        name: 'uxtheme.dll',
+        device: 'Lannister',
+        path: '\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll',
+        status: 'available',
+      },
+      {
+        name: 'cryptbase.dll',
+        device: 'Martell',
+        path: '\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll',
+        status: 'scheduled',
+      },
+      {
+        name: '7za.exe',
+        device: 'Baratheon',
+        path: '\\Device\\HarddiskVolume1\\temp\\7za.exe',
+        status: 'scheduled',
+      },
+    ];
+    return data;
+  }
+}
---
+++ src/app/table.ts
@@ -0,0 +1,6 @@
+export interface Table {
+  name: string;
+  device: string;
+  path: string;
+  status: string;
+}
